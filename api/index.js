3cde3d0e43902083ab140e809029d7146577d4b8
mohssin <barcalover016@gmail.com>
2023-09-26 13:56:19 +0100

    adding cors

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORt || 3090;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
